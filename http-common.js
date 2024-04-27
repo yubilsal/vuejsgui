c800f5d8cb1ca80632c1362a382387b3d1e01ef3
Yusuf Bilge SALDAMLI <yubilsal@gmail.com>
2024-04-28 02:34:04 +0300

    testttttttttttttt

--- http-common.js
+++ http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://springboot-yubilsal-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com",
+    "http://devfile-sample-java-springboot-basic-git-yubilsal-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com",
   headers: {
     "Content-Type": "application/json",
   },
